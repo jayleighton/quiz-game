99f8ce6d175a8a16b7c1e40bdec2ca729ed5e931
jayleighton <>
2023-12-19 10:40:34 +0000

    Add basic API call for questions. Unescape HTML characters

---
+++ question-data.py
@@ -0,0 +1,37 @@
+import requests
+import html
+from pprint import pprint
+
+TRIVIA_URL = 'https://opentdb.com/api.php'
+
+CATEGORIES = {
+    'General Knowledge': 9,
+    'Entertainment: Film': 11,
+    'Entertainment: Television': 14,
+    'Entertainment: Music': 12,
+    'Sports': 21,
+    'Geography': 22,
+    'Animals': 27,
+    'History': 23,
+    'Mythology': 20,
+    'Science and Nature': 17
+}
+
+def get_questions(number_of_questions, question_category):
+    params = {
+        'amount': number_of_questions,
+        'category': question_category,
+        'type': 'boolean'
+    }
+
+    result = requests.get(url=TRIVIA_URL, params=params)
+    return result.json()['results']
+
+
+questions = get_questions(10, 9)
+for question in questions:
+    answer = question['correct_answer']
+    question = html.unescape(question['question'])
+    print(question)
+    print(answer)
+
