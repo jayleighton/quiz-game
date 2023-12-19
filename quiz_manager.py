8685cca13269a127365bb0833c9078ac4b3b9791
jayleighton <>
2023-12-19 14:35:08 +0000

    Add code to check answer. Handle exception when a blank answer is submitted

--- quiz_manager.py
+++ quiz_manager.py
@@ -1,5 +1,5 @@
 from question_object import Question
-
+import os
 
 class QuizManager:
     def __init__(self, question_list):
@@ -7,6 +7,7 @@ class QuizManager:
         self.question_number = 0
         self.question_list = question_list
         self.current_quesion = None
+        os.system('clear')
     
     def has_question(self):
         return self.question_number < len(self.question_list)
@@ -14,5 +15,22 @@ class QuizManager:
     def next_question(self):
         self.current_quesion = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q. {self.question_number}. {self.current_quesion.question_text}. True/False\n")
+        while True:
+            try:
+                user_answer = input(f"Q.{self.question_number}: {self.current_quesion.question_text}. True/False\n")
+                if len(user_answer) == 0:
+                    raise ValueError()
+            except ValueError:
+                os.system('clear')
+                print("Invalid answer supplied.\nPlease try again\n")
+            else:
+                self.check_answer(user_answer)
+                break
 
+    def check_answer(self, users_answer):
+        correct_answer = self.current_quesion.question_answer
+        if users_answer[0].lower() == correct_answer[0].lower():
+            print("Well Done! That is correct\n")
+            self.score += 1
+        else:
+            print(f"Incorrect! The correct answer was: {correct_answer}\n")
