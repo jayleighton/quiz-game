878b174f1b369c4e2ac15aa33d8042e093d1689d
jayleighton <>
2023-12-19 11:18:43 +0000

    Add function to get question count from user and validate input

--- question_data.py
+++ question_data.py
@@ -1,5 +1,15 @@
 import requests
 from pprint import pprint
+import os
+
+LOGO = """
+         ______     __  __     __     ______        ______     ______     __    __     ______    
+        /\  __ \   /\ \/\ \   /\ \   /\___  \      /\  ___\   /\  __ \   /\ "-./  \   /\  ___\   
+        \ \ \/\_\  \ \ \_\ \  \ \ \  \/_/  /__     \ \ \__ \  \ \  __ \  \ \ \-./\ \  \ \  __\   
+         \ \___\_\  \ \_____\  \ \_\   /\_____\     \ \_____\  \ \_\ \_\  \ \_\ \ \_\  \ \_____\ 
+          \/___/_/   \/_____/   \/_/   \/_____/      \/_____/   \/_/\/_/   \/_/  \/_/   \/_____/ 
+                                                                                         
+       """
 
 TRIVIA_URL = 'https://opentdb.com/api.php'
 
@@ -16,6 +26,7 @@ CATEGORIES = {
     'Science and Nature': 17
 }
 
+
 def get_questions(number_of_questions, question_category):
     params = {
         'amount': number_of_questions,
@@ -27,5 +38,23 @@ def get_questions(number_of_questions, question_category):
     return result.json()['results']
 
 
-
+def get_question_count():
+    print(LOGO)
+    print("Welcome to the Quiz Game")
+    while True:
+        
+        try:
+            question_amount = int(input("How many questions would you like?:\nEnter a number between 10 and 40, or 0 to quit\n"))
+            if question_amount == 0:
+                print('Exiting')
+                break
+            if question_amount < 10 or question_amount > 40:
+                raise ValueError()
+            return question_amount
+        except ValueError:
+            os.system('clear')
+            print(LOGO)
+            print("Please enter a valid value between 10 and 40")
+            
+            
 
--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 import question_data as QD 
 import html
 
-questions = QD.get_questions(10, 9)
-for question in questions:
-    answer = question['correct_answer']
-    question = html.unescape(question['question'])
-    print(question)
-    print(answer)
+number_of_questions = QD.get_question_count()
+# if len(CATEGORIES) > 0:
+#             count = 1
+#             for category in CATEGORIES:
+#                 print(f"{count}. {category}")
+#                 count += 1
+
+# print(len(QD.CATEGORIES))
+
+# questions = QD.get_questions(10, 9)
+# for question in questions:
+#     answer = question['correct_answer']
+#     question = html.unescape(question['question'])
+#     print(question)
+#     print(answer)
